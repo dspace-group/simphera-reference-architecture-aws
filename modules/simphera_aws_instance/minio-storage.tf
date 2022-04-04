0ea79de366369577bc72b8437eedcc98eeb6607a
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2022-04-04 11:34:42 +0200

    chore: swap out iam policy for minio into templatefile

--- modules/simphera_aws_instance/minio-storage.tf
+++ modules/simphera_aws_instance/minio-storage.tf
@@ -19,43 +19,18 @@ resource "aws_iam_role" "minio_iam_role" {
     ]
   })
 }
-resource "aws_iam_role_policy" "s3_access_policy" {
-  name = "${var.infrastructurename}-s3-policy"
-  role = aws_iam_role.minio_iam_role.id
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:GetBucketLocation"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::*"
-        ]
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "s3:PutObject",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:DeleteObject",
-          "s3:HeadBucket"
-        ],
-        "Resource" : [
-          "arn:aws:s3:::${var.infrastructurename}",
-          "arn:aws:s3:::${var.infrastructurename}/*"
-        ]
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : "s3:ListAllMyBuckets",
-        "Resource" : "*"
-      }
-    ]
-  })
+
+resource "aws_iam_policy" "minio_policy" {
+  name        = "${var.infrastructurename}-s3-policy"
+  description = "Allows access to S3 bucket."
+  policy      = templatefile("${path.module}/templates/minio-policy.json", { bucket = var.infrastructurename })
 }
+
+resource "aws_iam_role_policy_attachment" "minio_policy_attachment" {
+  role       = aws_iam_role.minio_iam_role.name
+  policy_arn = aws_iam_policy.minio_policy.arn
+}
+
 resource "kubernetes_service_account" "minio_service_account" {
   metadata {
     name      = local.minio_serviceaccount
@@ -68,6 +43,18 @@ resource "kubernetes_service_account" "minio_service_account" {
 resource "aws_s3_bucket" "bucket" {
   bucket = var.infrastructurename
   acl    = "private"
+  versioning {
+    enabled = true
+  }
+}
 
+
+resource "aws_s3_bucket_public_access_block" "bucket_access_block" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
