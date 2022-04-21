76d23b7f5b7364cdf486a98abee0ff048e79a5ff
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2022-04-21 16:33:55 +0200

    chore: use more stable connection method

--- main.tf
+++ main.tf
@@ -39,12 +39,16 @@ provider "kubernetes" {
   experiments {
     manifest_resource = true
   }
-  config_path = "./kubeconfig_${module.eks.eks_cluster_id}"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "./kubeconfig_${module.eks.eks_cluster_id}"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
