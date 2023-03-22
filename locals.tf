91dd83b662db202bec27dc5619d232baf5090fbf
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2023-03-22 16:24:58 +0100

    output all s3 buckets

--- locals.tf
+++ locals.tf
@@ -7,11 +7,15 @@ locals {
   license_server_instance_id                = var.licenseServer ? split("instance/", aws_instance.license_server[0].arn)[1] : ""
   license_server_role                       = "${local.infrastructurename}-license-server-role"
   license_server_policy                     = "${local.infrastructurename}-license-server-policy"
-  license_server_bucket                     = "${local.infrastructurename}-license-server-bucket"
+  license_server_bucket_name                = "${local.infrastructurename}-license-server-bucket"
   license_server                            = "${local.infrastructurename}-license-server"
   license_server_instance_profile           = "${local.infrastructurename}-license-server-instance-profile"
   flowlogs_cloudwatch_loggroup              = "/aws/vpc/${module.eks.eks_cluster_id}"
   patch_manager_cloudwatch_loggroup_scan    = "/aws/ssm/${module.eks.eks_cluster_id}/scan"
   patch_manager_cloudwatch_loggroup_install = "/aws/ssm/${module.eks.eks_cluster_id}/install"
   patchgroupid                              = "${var.infrastructurename}-patch-group"
+  s3_instance_buckets                       = flatten([for name, instance in module.simphera_instance : instance.s3_buckets])
+  license_server_bucket                     = var.licenseServer ? [aws_s3_bucket.license_server_bucket[0].bucket] : []
+  s3_buckets                                = concat(local.s3_instance_buckets, [aws_s3_bucket.bucket_logs.bucket], local.license_server_bucket)
+
 }
