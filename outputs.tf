41fa5bfd90f14b8a65a059ce38b3c46becc5aa7c
Philipp Korth <philipp.korth.dev@mail.de>
2023-04-04 08:41:31 +0200

    Adds output of database endpoints

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,11 @@ output "database_identifiers" {
   value       = flatten([for name, instance in module.simphera_instance : instance.database_identifiers])
 }
 
+output "database_endpoints" {
+  description = "Identifiers of the SIMPHERA and Keycloak databases from all SIMPHERA instances."
+  value       = flatten([for name, instance in module.simphera_instance : instance.database_endpoints])
+}
+
 output "s3_buckets" {
   description = "S3 buckets from all SIMPHERA instances."
   value       = local.s3_buckets
