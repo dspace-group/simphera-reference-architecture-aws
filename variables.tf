bbcc8da5f4f45b96c65c7285b636b1360c1e280b
Philipp Korth <philipp.korth.dev@mail.de>
2023-05-31 14:22:59 +0200

    Updates node size for the gpu pool

--- variables.tf
+++ variables.tf
@@ -83,7 +83,7 @@ variable "gpuNodeCountMax" {
 variable "gpuNodeSize" {
   type        = string
   description = "The machine size of the nodes for the gpu job execution"
-  default     = "Standard_NC16as_T4_v3"
+  default     = "p3.2xlarge"
 }
 
 variable "licenseServer" {
