16abe8eaf56b6b245ee9538b68dfbe40374a8e46
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2023-10-05 14:41:30 +0200

    cache .terraform folder in precommit hook

--- hooks/terraform_docs.sh
+++ hooks/terraform_docs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 terraform-docs markdown table --output-file README.md --output-mode inject . 
-terraform-docs tfvars hcl . > terraform.tfvars.example
+terraform-docs tfvars hcl --description . > terraform.tfvars.example
+terraform-docs tfvars json . > terraform.json.example
