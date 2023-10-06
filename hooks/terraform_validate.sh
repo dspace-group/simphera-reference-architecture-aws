8736d7121bee8a12c9341d0f42e96932ec278a10
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2023-10-06 16:43:50 +0200

    debugging

--- hooks/terraform_validate.sh
+++ hooks/terraform_validate.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-
-ls -ahl
-
-export TF_PLUGIN_CACHE_DIR="/src/cache"
-echo $TF_PLUGIN_CACHE_DIR
-
-[ "$(ls -A /src/cache)" ] && echo "Not Empty" || terraform init
-#[ ! -d ".terraform" ] && terraform init -lock=false # Unfortunately, kubernetes-addons submodule must be downloaded
+[ ! -d ".terraform" ] && terraform init || echo "skipping terraform init"
 terraform validate
-
