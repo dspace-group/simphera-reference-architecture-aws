1bd2a3d506ecbc87d62156fbb9143392ce82ccc4
Simon Schwichtenberg <SimonSc@dspace.de>
2023-10-07 00:01:20 +0200

    use terraform docs config file

--- hooks/terraform_docs.sh
+++ hooks/terraform_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 terraform-docs markdown table --output-file README.md --output-mode inject . 
-terraform-docs tfvars hcl --description . > terraform.tfvars.example
-terraform-docs tfvars json . > terraform.json.example
+terraform-docs -c tfvars.hcl.terraform-docs.yml .
+terraform-docs -c tfvars.json.terraform-docs.yml .
