8c8baf617f5fca175723d1c63c17a1de05daa7b7
Simon Schwichtenberg <SSchwichtenberg@dspace.de>
2023-10-06 12:05:54 +0200

    testing cache

--- hooks/tflint.sh
+++ hooks/tflint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-tflint --init
+[ ! -d ".tflint.d" ] && tflint --init
 tflint
+[ $? -eq 0 ]  || exit $? # Pass the exit code from tflint
 tflint --config ../../.tflint.hcl --chdir ./modules/simphera_aws_instance
+[ $? -eq 0 ]  || exit $? # Pass the exit code from tflint
+
+
