153dc68100985d93b731146534a84dcb2b21a5c8
Philipp Korth <pkorth@dspace.de>
2023-11-03 13:46:44 +0100

    Adds tags to scaling groups

--- k8s.tf
+++ k8s.tf
@@ -53,3 +53,39 @@ module "eks-addons" {
   cluster_autoscaler_helm_config = var.cluster_autoscaler_helm_config
   depends_on                     = [module.eks.managed_node_groups]
 }
+
+
+data "aws_eks_node_group" "execnodes" {
+  cluster_name    = local.infrastructurename
+  node_group_name = replace(module.eks.managed_node_groups[0]["execnodes"]["managed_nodegroup_id"][0], "${local.infrastructurename}:", "")
+
+}
+
+data "aws_eks_node_group" "gpuexecnodes" {
+  count           = var.gpuNodePool ? 1 : 0
+  cluster_name    = local.infrastructurename
+  node_group_name = replace(module.eks.managed_node_groups[0]["gpuexecnodes"]["managed_nodegroup_id"][0], "${local.infrastructurename}:", "")
+}
+
+resource "aws_autoscaling_group_tag" "execnodes" {
+  autoscaling_group_name = data.aws_eks_node_group.execnodes.resources[0].autoscaling_groups[0].name
+
+  tag {
+    key   = "k8s.io/cluster-autoscaler/node-template/label/purpose"
+    value = "execution"
+
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_autoscaling_group_tag" "gpuexecnodes" {
+  count                  = var.gpuNodePool ? 1 : 0
+  autoscaling_group_name = data.aws_eks_node_group.gpuexecnodes[0].resources[0].autoscaling_groups[0].name
+
+  tag {
+    key   = "k8s.io/cluster-autoscaler/node-template/label/purpose"
+    value = "gpu"
+
+    propagate_at_launch = true
+  }
+}
