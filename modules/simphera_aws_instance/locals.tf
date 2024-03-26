774b57a9c750150ceb50f194ebe465478516eb9b
Ante Marin <amarin@dspace.hr>
2024-03-26 13:42:03 +0100

    reworked keycloak refferal

--- modules/simphera_aws_instance/locals.tf
+++ modules/simphera_aws_instance/locals.tf
@@ -4,7 +4,7 @@ locals {
   secret_postgres_username = "dbuser" # username is hardcoded because changing the username forces replacement of the db instance
   secrets                  = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)
   instancename             = join("-", [var.infrastructurename, var.name])
-  backup_resources         = concat([aws_db_instance.simphera.arn], [aws_db_instance.keycloak[count.index].arn], [aws_s3_bucket.bucket.arn])
+  backup_resources         = concat([aws_db_instance.simphera.arn], var.enableKeycloak ? [aws_db_instance.keycloak[0].arn] : [], [aws_s3_bucket.bucket.arn])
   db_simphera_id           = "${local.instancename}-simphera"
   db_keycloak_id           = "${local.instancename}-keycloak"
   backup_vault_name        = "${local.instancename}-backup-vault"
