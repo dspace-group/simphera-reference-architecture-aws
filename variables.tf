96b40361982e70b7e8bd6815af6641369a616b6f
Ante Marin <amarin@dspace.hr>
2024-04-09 15:52:31 +0200

    Formatting fix

--- variables.tf
+++ variables.tf
@@ -258,5 +258,5 @@ variable "cloudwatch_retention" {
 variable "cluster_autoscaler_helm_config" {
   type        = any
   description = "Cluster Autoscaler Helm Config"
-  default     = {version = "9.28.0"}
+  default     = { version = "9.28.0" }
 }
