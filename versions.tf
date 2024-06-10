727600297536bcf275edf11c522b42a6773d1461
Ante Marin <amarin@dspace.hr>
2024-06-10 11:47:31 +0200

    upgraded terraform-aws-provider and aws_vpc module to latest available versions

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.72, < 5.0.0"
+      version = ">= 5.37.0"
       # beginning with version 5.0 some arguments are removed from resource "aws_vpc".
     }
     kubernetes = {
