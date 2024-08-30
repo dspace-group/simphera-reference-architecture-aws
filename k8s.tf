a597cbca171117aceab08e79da5878a1ec9646d3
johannessc <JSchuermeyer@dspace.de>
2024-08-30 09:21:13 +0200

    remove dependson

--- k8s.tf
+++ k8s.tf
@@ -50,7 +50,7 @@ module "eks-addons" {
   }
 
   cluster_autoscaler_helm_config = var.cluster_autoscaler_helm_config
-  depends_on                     = [module.eks.managed_node_groups]
+  #depends_on                     = [module.eks.managed_node_groups]
 }
 
 
