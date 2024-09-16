0d824fd50a362b11586f96e124fbcae9b02446cd
Valentin Radicevic <vradicevic@dspace.hr>
2024-09-16 14:54:15 +0200

    Prettify

--- variables.tf
+++ variables.tf
@@ -208,12 +208,6 @@ variable "rtMaps_link" {
   default     = "http://dl.intempora.com/RTMaps4/rtmaps_4.9.0_ubuntu1804_x86_64_release.tar.bz2"
 }
 
-# variable "enable_ingress_nginx" {
-#   type        = bool
-#   description = "Enable Ingress Nginx add-on"
-#   default     = false
-# }
-
 variable "map_accounts" {
   type        = list(string)
   description = "Additional AWS account numbers to add to the aws-auth ConfigMap"
