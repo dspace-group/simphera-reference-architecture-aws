39805e62dea7831a016999317ab01978364f2783
Valentin Radicevic <vradicevic@dspace.hr>
2024-10-17 08:31:29 +0200

    Remove unused input from addon_context

--- k8s-eks-addons.tf
+++ k8s-eks-addons.tf
@@ -6,15 +6,14 @@ module "k8s_eks_addons" {
   coredns_config            = var.coredns_config
 
   addon_context = {
-    aws_caller_identity_account_id     = data.aws_caller_identity.current.account_id
-    aws_partition_id                   = data.aws_partition.current.partition
-    aws_region_name                    = data.aws_region.current.name
-    eks_cluster_endpoint               = module.eks.eks_cluster_endpoint
-    eks_cluster_id                     = module.eks.eks_cluster_id
-    eks_cluster_version                = module.eks.eks_cluster_version
-    cluster_certificate_authority_data = module.eks.eks_cluster_certificate_authority_data
-    eks_oidc_issuer_url                = replace(module.eks.eks_oidc_issuer_url, "https://", "")
-    tags                               = var.tags
+    aws_caller_identity_account_id = data.aws_caller_identity.current.account_id
+    aws_partition_id               = data.aws_partition.current.partition
+    aws_region_name                = data.aws_region.current.name
+    eks_cluster_endpoint           = module.eks.eks_cluster_endpoint
+    eks_cluster_id                 = module.eks.eks_cluster_id
+    eks_cluster_version            = module.eks.eks_cluster_version
+    eks_oidc_issuer_url            = replace(module.eks.eks_oidc_issuer_url, "https://", "")
+    tags                           = var.tags
   }
 
   depends_on = [module.eks.eks_cluster_arn]
