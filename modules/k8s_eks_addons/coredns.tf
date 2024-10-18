5790571675e652e06fdb5a2870ef29967372050a
Valentin Radicevic <vradicevic@dspace.hr>
2024-10-18 08:47:57 +0200

    Change deprecated inputs

--- modules/k8s_eks_addons/coredns.tf
+++ modules/k8s_eks_addons/coredns.tf
@@ -9,13 +9,14 @@ data "aws_eks_addon_version" "coredns" {
 }
 
 resource "aws_eks_addon" "coredns" {
-  count                = var.coredns_config.enable ? 1 : 0
-  cluster_name         = var.addon_context.eks_cluster_id
-  addon_name           = local.coredns_addon_name
-  addon_version        = data.aws_eks_addon_version.coredns[0].version
-  preserve             = true
-  resolve_conflicts    = "OVERWRITE"
-  configuration_values = var.coredns_config.configuration_values
-  tags                 = merge(var.addon_context.tags)
+  count                       = var.coredns_config.enable ? 1 : 0
+  cluster_name                = var.addon_context.eks_cluster_id
+  addon_name                  = local.coredns_addon_name
+  addon_version               = data.aws_eks_addon_version.coredns[0].version
+  preserve                    = true
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  configuration_values        = var.coredns_config.configuration_values
+  tags                        = merge(var.addon_context.tags)
 
 }
