e35fc65fe9e420631fa2f9850674b6e39bfd4735
Renjith Purushothaman Nair <RNair@dspace.de>
2024-11-08 08:25:28 +0100

    blank line at the end of the file

--- modules/k8s_eks_addons/ebs-csi.tf
+++ modules/k8s_eks_addons/ebs-csi.tf
@@ -61,3 +61,4 @@ resource "aws_iam_role_policy_attachment" "ebs_csi_driver_policy_attachment" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   role       = aws_iam_role.ebs_csi_driver_role.name
 }
+
