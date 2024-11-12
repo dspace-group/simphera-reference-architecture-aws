989ccfc2bc452e2fc9e304ec8ff22e38136b85ab
Mario Gluhakovic <mgluhakovic@dspace.hr>
2024-11-12 15:46:12 +0100

    Add missing Amazons3CSIDriverPolicy

--- modules/k8s_eks_addons/s3-csi.tf
+++ modules/k8s_eks_addons/s3-csi.tf
@@ -15,44 +15,81 @@ resource "aws_eks_addon" "aws-mountpoint-s3-csi-driver" {
   cluster_name  = var.addon_context.eks_cluster_id
   addon_name    = local.aws_s3_csi_addon_name
   addon_version = data.aws_eks_addon_version.aws-mountpoint-s3-csi-driver[0].version
-  # service_account_role_arn    = aws_iam_role.s3_csi_driver_role[0].arn
+  service_account_role_arn    = aws_iam_role.s3_csi_driver_role[0].arn
   preserve                    = true
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
   tags                        = var.addon_context.tags
 }
 
-# resource "aws_iam_role" "s3_csi_driver_role" {
-#   count       = var.s3_csi_config.enable ? 1 : 0
-#   name        = format("%s-%s-%s", var.addon_context.eks_cluster_id, trimsuffix(local.aws_s3_csi_service_account, "-sa"), "irsa")
-#   description = "AWS IAM Role for the Kubernetes service account ${local.aws_s3_csi_service_account}."
+resource "aws_iam_role" "s3_csi_driver_role" {
+  count       = var.s3_csi_config.enable ? 1 : 0
+  name        = format("%s-%s-%s", var.addon_context.eks_cluster_id, trimsuffix(local.aws_s3_csi_service_account, "-sa"), "irsa")
+  description = "AWS IAM Role for the Kubernetes service account ${local.aws_s3_csi_service_account}."
 
-#   assume_role_policy = jsonencode({
-#     "Version" : "2012-10-17",
-#     "Statement" : [
-#       {
-#         "Effect" : "Allow",
-#         "Principal" : {
-#           "Federated" : "arn:${var.addon_context.aws_partition_id}:iam::${var.addon_context.aws_caller_identity_account_id}:oidc-provider/${var.addon_context.eks_oidc_issuer_url}"
-#         },
-#         "Action" : "sts:AssumeRoleWithWebIdentity",
-#         "Condition" : {
-#           "StringLike" : {
-#             "${var.addon_context.eks_oidc_issuer_url}:sub" : "system:serviceaccount:${local.aws_s3_csi_namespace}:${local.aws_s3_csi_service_account}",
-#             "${var.addon_context.eks_oidc_issuer_url}:aud" : "sts.amazonaws.com"
-#           }
-#         }
-#       }
-#     ]
-#   })
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:${var.addon_context.aws_partition_id}:iam::${var.addon_context.aws_caller_identity_account_id}:oidc-provider/${var.addon_context.eks_oidc_issuer_url}"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringLike" : {
+            "${var.addon_context.eks_oidc_issuer_url}:sub" : "system:serviceaccount:${local.aws_s3_csi_namespace}:${local.aws_s3_csi_service_account}",
+            "${var.addon_context.eks_oidc_issuer_url}:aud" : "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
 
-#   force_detach_policies = true
+  force_detach_policies = true
 
-#   tags = var.addon_context.tags
-# }
+  tags = var.addon_context.tags
+}
 
-# resource "aws_iam_role_policy_attachment" "s3_csi_driver_policy_attachment" {
-#   count      = var.s3_csi_config.enable ? 1 : 0
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/Amazons3CSIDriverPolicy"
-#   role       = aws_iam_role.s3_csi_driver_role[0].name
-# }
+resource "aws_iam_policy" "Amazons3CSIDriverPolicy" {
+  count       = var.s3_csi_config.enable ? 1 : 0
+  name        = "Amazons3CSIDriverPolicy"
+  description = "Amazons3CSIDriverPolicy"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "MountpointFullBucketAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:ListBucket"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        },
+        {
+            "Sid": "MountpointFullObjectAccess",
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject",
+                "s3:AbortMultipartUpload",
+                "s3:DeleteObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::*"
+            ]
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_csi_driver_policy_attachment" {
+  count      = var.s3_csi_config.enable ? 1 : 0
+  policy_arn = aws_iam_policy.Amazons3CSIDriverPolicy[0].arn
+  role       = aws_iam_role.s3_csi_driver_role[0].name
+
+  depends_on = [ aws_iam_policy.Amazons3CSIDriverPolicy ]
+}
