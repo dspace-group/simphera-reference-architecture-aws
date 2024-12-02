c6db00526bb7f7a34d6e384983b5feaaa922a942
Christian Bergen <cbergen@dspace.de>
2024-12-02 15:55:35 +0100

    replace namespace creation resource

--- modules/k8s_eks_addons/gpu-operator.tf
+++ modules/k8s_eks_addons/gpu-operator.tf
@@ -1,17 +1,10 @@
-resource "kubernetes_namespace_v1" "gpu_operator" {
-  count = var.gpu_operator_config.enable ? 1 : 0
-
-  metadata {
-    name = "gpu-operator"
-  }
-}
-
 resource "helm_release" "gpu_operator" {
   count = var.gpu_operator_config.enable ? 1 : 0
 
   namespace         = kubernetes_namespace_v1.gpu_operator[0].metadata[0].name
   name              = "gpu-operator"
   chart             = "gpu-operator"
+  create_namespace  = true
   repository        = var.gpu_operator_config.helm_repository
   version           = var.gpu_operator_config.helm_version
   description       = "The GPU operator HelmChart deployment configuration"
