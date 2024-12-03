e775b2b741c081091c949623f9e95701ff26ace8
Christian Bergen <cbergen@dspace.de>
2024-12-03 08:46:27 +0100

    remove default

--- variables.tf
+++ variables.tf
@@ -382,8 +382,5 @@ YAML
     )
   })
   description = "Input configuration for GPU operator deployed with helm release. By setting key 'enable' to 'true', GPU operator will be deployed. 'helm_repository' is an URL for the repository of GPU operator helm chart, where 'helm_version' is its respective version of a chart. 'chart_values' is used for changing default values.yaml of an GPU operator chart."
-  default = {
-    enable = false
-  }
 }
 
