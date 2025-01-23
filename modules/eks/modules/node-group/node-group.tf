550e399f083002d6394f1efdf6267f6b265ab2c6
Valentin Radicevic <vradicevic@dspace.hr>
2025-01-23 16:08:39 +0100

    Fix node group references

--- modules/eks/modules/node-group/node-group.tf
+++ modules/eks/modules/node-group/node-group.tf
@@ -64,7 +64,7 @@ resource "aws_eks_node_group" "node_group" {
   tags = local.common_tags
   depends_on = [
     aws_iam_role.node_group,
-    aws_iam_instance_profilenode_group,
+    aws_iam_instance_profile.node_group,
     aws_iam_role_policy_attachment.node_group
   ]
 }
