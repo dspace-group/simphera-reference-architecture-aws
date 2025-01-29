9ff5c71053619cd90cc4c392b12eded32066f0d3
Valentin Radicevic <vradicevic@dspace.hr>
2025-01-29 09:09:28 +0100

    Fix tfsec by falling to default values

--- modules/eks/cluster.tf
+++ modules/eks/cluster.tf
@@ -6,8 +6,7 @@ resource "aws_eks_cluster" "eks" {
   bootstrap_self_managed_addons = false
 
   vpc_config {
-    subnet_ids              = var.subnet_ids
-    endpoint_private_access = false
+    subnet_ids = var.subnet_ids
   }
 
   kubernetes_network_config {
