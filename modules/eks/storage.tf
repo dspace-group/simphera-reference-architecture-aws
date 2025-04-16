17b98ffab57800577bce62adb60cf1c105da44ef
Soham Kalghatgi <skalghatgi@dspace.de>
2025-04-16 10:33:56 +0200

    gp3 storage class set as default

--- modules/eks/storage.tf
+++ modules/eks/storage.tf
@@ -1,6 +1,9 @@
 resource "kubernetes_storage_class_v1" "gp3" {
   metadata {
     name = "gp3"
+    annotations = {
+      "storageclass.kubernetes.io/is-default-class" = "true"
+    }
   }
   storage_provisioner = "ebs.csi.aws.com"
   volume_binding_mode = "WaitForFirstConsumer"
